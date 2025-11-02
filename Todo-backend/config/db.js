f4039439e20b50068418aae728df3ae6dcccd17e
sharmasatyam121104-devloper <sharma.satyam121104@gmail.com>
2025-11-02 23:06:43 +0530

    add something 5

--- Todo-backend/config/db.js
+++ Todo-backend/config/db.js
@@ -1,15 +1,22 @@
 import mongoose from "mongoose";
 import "colors";
+import dotenv from "dotenv";
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    const mongoURI = `${process.env.MONGO_DB_URI}/todo-backend`;
+    const mongoURI = process.env.MONGO_DB_URI;
 
-    await mongoose.connect(mongoURI);
-    console.log("MongoDB Connected Successfully!".blue.bold);
-    console.log("Connecting to:", mongoURI);
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(" MongoDB Connected Successfully!".blue.bold);
+    console.log(" Connecting to:", mongoURI.green);
   } catch (error) {
-    console.log(`DB Connection Failed: ${error.message}`.red.bold);
+    console.log(` DB Connection Failed: ${error.message}`.red.bold);
+    process.exit(1);
   }
 };
 
